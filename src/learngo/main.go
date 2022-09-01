8ee470ec330d5d6e8fb73e982e3dd9c905e60480
meohyun24 <eogus1954@naver.com>
2022-09-02 01:26:35 +0900

    2022.09.02. 01:26

--- src/learngo/main.go
+++ src/learngo/main.go
@@ -8,7 +8,9 @@ import (
 	// goarray "learngo/array"
 	// gomap "learngo/map"
 	// gostruct "learngo/struct"
-	accounts "learngo/banking"
+	//accounts "learngo/banking"
+	"fmt"
+	mydict "learngo/dict"
 )
 
 func main() {
@@ -21,7 +23,40 @@ func main() {
 	// goarray.Makearray()
 	// gomap.Makemap()
 	// gostruct.Makestruct()
-	account := accounts.NewAccount("대현")
-	account.Deposit(10)
+	//account := accounts.NewAccount("대현")
+	// account.Deposit(10)
+	// err := account.Withdraw(20)
+	// if err != nil{
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(account.Balance())
+	// account.ChangeOwner("현다이")
+	// fmt.Println(account.Owner())
+	// fmt.Println(account)
+	dictonary := mydict.Dictionary{"First": "first word"}
+	//fmt.Println(dictonary)
+	// value, err := dictonary.Search("Second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(value)
+	// }
+	adderr := dictonary.Add("Second", "second word")
+	if adderr != nil {
+		fmt.Println(adderr)
+	}
+
+	// updateerr := dictonary.Update("First", "Second")
+	// if updateerr != nil {
+	// 	fmt.Println(updateerr)
+	// }
+	// fmt.Println(dictonary)
+
+	err := dictonary.Worddelete("First")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(dictonary)
+	}
 
 }
